47d3b335274893381cd4759abced5014e99f9cee
Adam Fisk <a@littleshoot.org>
2012-10-24 18:11:55 -0700

    Added thread numbering and fixed formatting

--- src/main/java/org/littleshoot/util/DaemonThreadFactory.java
+++ src/main/java/org/littleshoot/util/DaemonThreadFactory.java
@@ -5,24 +5,26 @@ import java.util.concurrent.ThreadFactory;
 /**
  * A thread factory that creates daemon threads.
  */
-public class DaemonThreadFactory implements ThreadFactory
-    {
+public class DaemonThreadFactory implements ThreadFactory {
 
-    private final String m_name;
+    private final String name;
+    
+    private volatile int count = 0;
 
     /**
      * Creates a new daemon thread factory.
      * 
-     * @param name The prefix name for new threads.
+     * @param name
+     *            The prefix name for new threads.
      */
-    public DaemonThreadFactory(final String name)
-        {
-        this.m_name = name;
-        }
-    
-    public Thread newThread(final Runnable r)
-        {
-        return new DaemonThread(r, m_name+"-"+r.hashCode());
-        }
-
+    public DaemonThreadFactory(final String name) {
+        this.name = name;
     }
+
+    @Override
+    public Thread newThread(final Runnable r) {
+        count++;
+        return new DaemonThread(r, name + "-" + r.hashCode() +"-"+count);
+    }
+
+}
