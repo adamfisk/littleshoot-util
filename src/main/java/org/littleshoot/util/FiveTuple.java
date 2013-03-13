2d91c6293c0e82ffe4d3457def525f70f06273c4
Adam Fisk <a@littleshoot.org>
2013-03-13 15:53:30 -0700

    hashcode and equals

--- src/main/java/org/littleshoot/util/FiveTuple.java
+++ src/main/java/org/littleshoot/util/FiveTuple.java
@@ -42,4 +42,39 @@ public class FiveTuple {
         return "FiveTuple [local=" + local + ", remote=" + remote
                 + ", protocol=" + protocol + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((local == null) ? 0 : local.hashCode());
+        result = prime * result
+                + ((protocol == null) ? 0 : protocol.hashCode());
+        result = prime * result + ((remote == null) ? 0 : remote.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FiveTuple other = (FiveTuple) obj;
+        if (local == null) {
+            if (other.local != null)
+                return false;
+        } else if (!local.equals(other.local))
+            return false;
+        if (protocol != other.protocol)
+            return false;
+        if (remote == null) {
+            if (other.remote != null)
+                return false;
+        } else if (!remote.equals(other.remote))
+            return false;
+        return true;
+    }
 }
