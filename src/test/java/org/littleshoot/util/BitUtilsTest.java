891ca39a313c6490e1dab6f871d615e9bc183ca7
Leah X Schmidt <leahxschmidt@gmail.com>
2013-05-02 23:14:55 -0700

    add the converse of the byte-array functions

--- src/test/java/org/littleshoot/util/BitUtilsTest.java
+++ src/test/java/org/littleshoot/util/BitUtilsTest.java
@@ -11,7 +11,8 @@ public class BitUtilsTest extends TestCase
     public void testLongToByteArray() throws Exception
         {
         byte[] bytes = BitUtils.toByteArray(1L);
-        
+        assertEquals(1L, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -20,9 +21,10 @@ public class BitUtilsTest extends TestCase
         assertEquals(0, bytes[5]);
         assertEquals(0, bytes[6]);
         assertEquals(1, bytes[7]);
-        
+
         bytes = BitUtils.toByteArray(256L);
-        
+        assertEquals(256L, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -31,9 +33,10 @@ public class BitUtilsTest extends TestCase
         assertEquals(0, bytes[5]);
         assertEquals(1, bytes[6]);
         assertEquals(0, bytes[7]);
-        
+
         bytes = BitUtils.toByteArray(127L);
-        
+        assertEquals(127L, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -42,9 +45,10 @@ public class BitUtilsTest extends TestCase
         assertEquals(0, bytes[5]);
         assertEquals(0, bytes[6]);
         assertEquals(127, bytes[7]);
-        
+
         bytes = BitUtils.toByteArray(0xffffL);
-        
+        assertEquals(0xffffL, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -53,9 +57,10 @@ public class BitUtilsTest extends TestCase
         assertEquals(0, bytes[5]);
         assertEquals(-1, bytes[6]);
         assertEquals(-1, bytes[7]);
-        
+
         bytes = BitUtils.toByteArray(0x01ffffL);
-        
+        assertEquals(0x01ffff, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -64,9 +69,10 @@ public class BitUtilsTest extends TestCase
         assertEquals(1, bytes[5]);
         assertEquals(-1, bytes[6]);
         assertEquals(-1, bytes[7]);
-        
+
         bytes = BitUtils.toByteArray(0x09ffffffL);
-        
+        assertEquals(0x09ffffffL, BitUtils.byteArrayToLong(bytes));
+
         assertEquals(0, bytes[0]);
         assertEquals(0, bytes[1]);
         assertEquals(0, bytes[2]);
@@ -75,5 +81,79 @@ public class BitUtilsTest extends TestCase
         assertEquals(-1, bytes[5]);
         assertEquals(-1, bytes[6]);
         assertEquals(-1, bytes[7]);
+
+
+        bytes = BitUtils.toByteArray(0x09ffffffffL);
+        assertEquals(0x09ffffffffL, BitUtils.byteArrayToLong(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(0, bytes[1]);
+        assertEquals(0, bytes[2]);
+        assertEquals(9, bytes[3]);
+        assertEquals(-1, bytes[4]);
+        assertEquals(-1, bytes[5]);
+        assertEquals(-1, bytes[6]);
+        assertEquals(-1, bytes[7]);
+
+
+        }
+
+    public void testIntegerToByteArray() throws Exception
+    {
+        byte[] bytes = BitUtils.toByteArray(1);
+        assertEquals(1, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(0, bytes[1]);
+        assertEquals(0, bytes[2]);
+        assertEquals(1, bytes[3]);
+
+        bytes = BitUtils.toByteArray(256);
+        assertEquals(256, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(0, bytes[1]);
+        assertEquals(1, bytes[2]);
+        assertEquals(0, bytes[3]);
+
+        bytes = BitUtils.toByteArray(127);
+        assertEquals(127, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(0, bytes[1]);
+        assertEquals(0, bytes[2]);
+        assertEquals(127, bytes[3]);
+
+        bytes = BitUtils.toByteArray(0xffff);
+        assertEquals(0xffff, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(0, bytes[1]);
+        assertEquals(-1, bytes[2]);
+        assertEquals(-1, bytes[3]);
+
+        bytes = BitUtils.toByteArray(0x01ffff);
+        assertEquals(0x01ffff, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(0, bytes[0]);
+        assertEquals(1, bytes[1]);
+        assertEquals(-1, bytes[2]);
+        assertEquals(-1, bytes[3]);
+
+        bytes = BitUtils.toByteArray(0x09ffffff);
+        assertEquals(0x09ffffff, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(9, bytes[0]);
+        assertEquals(-1, bytes[1]);
+        assertEquals(-1, bytes[2]);
+        assertEquals(-1, bytes[3]);
+
+        bytes = BitUtils.toByteArray(0xffffffff);
+        assertEquals(0xffffffff, BitUtils.byteArrayToInteger(bytes));
+
+        assertEquals(-1, bytes[0]);
+        assertEquals(-1, bytes[1]);
+        assertEquals(-1, bytes[2]);
+        assertEquals(-1, bytes[3]);
         }
     }
